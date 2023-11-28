74ad974d9d3c01234e1231c2e6aae8a17c4f6670
GrowlyX <growlyxdev@gmail.com>
2023-11-27 20:40:25 -0500

    chore: update tests

--- src/test/kotlin/io/liftgate/bus/dataflow/modules/APIRoutingKtTest.kt
+++ src/test/kotlin/io/liftgate/bus/dataflow/modules/APIRoutingKtTest.kt
@@ -1,6 +1,8 @@
 package io.liftgate.bus.dataflow.modules
 
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import io.liftgate.bus.dataflow.models.BusDataPackage
 import io.liftgate.bus.dataflow.module
@@ -8,12 +10,18 @@ import kotlin.test.Test
 
 class APIRoutingKtTest
 {
-
     @Test
     fun testPostApiV1Submitdatapackage() = testApplication {
         application {
             module()
         }
+
+        val client = createClient {
+            install(ContentNegotiation) {
+                json(json)
+            }
+        }
+
         client.post("/api/v1/submit-data-package/") {
             setBody(BusDataPackage(
                 busId = "hors",
